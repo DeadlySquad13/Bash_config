6cf12ceea24b3fd80cf838df9bf676b4991d30b4
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-08-21 19:34:13 +0000

    [Enh](SystemctlWrapper): Added --user variants.

--- _utilities/systemctlWrapper.sh
+++ _utilities/systemctlWrapper.sh
@@ -1,30 +1,72 @@
 # Sytemctl shorthands for common commands.
 # Source: alzee. “Answer to ‘How to Make Alias to Systemctl with Autocomplete.’” Ask Ubuntu, April 9, 2022. https://askubuntu.com/a/1401610.
-# ?: Always sudo, what about --user?
+# Do we need sudo in user mode?
+# Example:
+# # Will show status for docker service for current user.
+# systemctlWrapper u s docker
 systemctlWrapper() {
+    userMode=false
+
+    if [[ "$1" = "u" ]] || [[ "$1" = "user" ]]; then
+        shift;
+        userMode=true
+    fi
+
     case $1 in
         e)
             shift;
-            sudo systemctl enable --now "$@"
+            if [[ "$userMode" = true ]]; then
+                systemctl enable --now --user "$@"
+            else
+                sudo systemctl enable --now "$@"
+            fi
         ;;
         d)
             shift;
-            sudo systemctl disable --now "$@"
+            if [[ "$userMode" = true ]]; then
+                systemctl disable --now --user "$@"
+            else
+                sudo systemctl disable --now "$@"
+            fi
         ;;
         s)
             shift;
-            systemctl status "$@"
+            if [[ "$userMode" = true ]]; then
+                systemctl status --user "$@"
+            else
+                systemctl status "$@"
+            fi
         ;;
         S)
             shift;
-            sudo systemctl stop "$@"
+            if [[ "$userMode" = true ]]; then
+                systemctl start --user "$@"
+            else
+                sudo systemctl start "$@"
+            fi
+        ;;
+        x)
+            shift;
+            if [[ "$userMode" = true ]]; then
+                systemctl stop --user "$@"
+            else
+                sudo systemctl stop "$@"
+            fi
         ;;
         r)
             shift;
-            sudo systemctl restart "$@"
+            if [[ "$userMode" = true ]]; then
+                systemctl restart --user "$@"
+            else
+                sudo systemctl restart "$@"
+            fi
         ;;
         *)
-            systemctl "$@"
+            if [[ "$userMode" = true ]]; then
+                systemctl --user "$@"
+            else
+                systemctl "$@"
+            fi
         ;;
     esac
 }
