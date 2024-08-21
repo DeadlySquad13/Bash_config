1670b3214feaf69708add2f0978cb94472fb0bab
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-08-21 19:44:52 +0000

    [Enh](Ssh): Added rutube key.

--- sshAuthorization.sh
+++ sshAuthorization.sh
@@ -1,5 +1,5 @@
 export KEYS_PATH="$HOME/.ssh"
-declare -a keys=("$KEYS_PATH/Personal__DeadlySquad13_gitHub" "$KEYS_PATH/Personal__DeadlySquad13_gitLab");
+declare -a keys=("$KEYS_PATH/Personal__DeadlySquad13_gitHub" "$KEYS_PATH/Personal__DeadlySquad13_gitLab" "$KEYS_PATH/Work__Rutube_gitLab");
 
 # Init ssh agent and add necessary keys.
 if [[ -f ~/.bash/_utilities/addSshKeys.sh ]]; then
